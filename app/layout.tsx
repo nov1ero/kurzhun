75d3a0a347a176cdfc7ebb3e8d68096bfea1d694
noviero <78156339+nov1ero@users.noreply.github.com>
2026-05-17 20:54:19 +0600

    изменения в тексте

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Kurjun",
   description: "Archive of Stories",
+  verification: {
+    google: "u7tOZ0lPDqcGaFd-vk4QoPaSVbtTcpPlY2oLVZ68lkw",
+  },
 };
 
 export default function RootLayout({
